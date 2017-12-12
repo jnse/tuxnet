5e0cf6ec58b0c6959649136347d9a3378ac7cb50
John Sennesael <john@adminking.com>
2017-12-11 20:17:34 -0600

    Finish up barebones initial log implementation.

--- include/tuxnet/log.h
+++ include/tuxnet/log.h
@@ -14,11 +14,36 @@ namespace tuxnet
         static log* m_this_ptr;
         
         public:
-        
-            static log* get_ptr();
 
-            static void info(const std::string& message);
-            static void debug(const std::string& message);
+            /**
+             * Get pointer to log instance.
+             *
+             * @return Returns a pointer to the log object singleton.
+             *         Class is instantiated on first use.
+             **/
+            static log* get();
+
+            /**
+             * Log an informational message.
+             *
+             * @param message : Message to be logged.
+             **/
+            void info(const std::string& message);
+
+            /**
+             * Log a debug message.
+             *
+             * @param message : Message to be logged.
+             **/
+            void debug(const std::string& message);
+
+            /**
+             * Log an error message.
+             *
+             * @param message : Message to be logged.
+             **/
+            void error(const std::string& message);
+
 
     };
 
--- src/log.cpp
+++ src/log.cpp
@@ -1,21 +1,36 @@
+#include <iostream>
 #include "tuxnet/log.h"
 
 namespace tuxnet
 {
 
-    /// \TODO finish me.
-    log* log::get_ptr()
+    // Initialize log instance pointer.
+    log* log::m_this_ptr = 0;
+
+    // Get pointer to log instance.
+    log* log::get()
     {
         if (m_this_ptr == nullptr) m_this_ptr = new log();
         return m_this_ptr;
     }
 
+    //  Log an informational message.
     void log::info(const std::string& message)
     {
+        std::cout << message << std::endl;
     }
 
+    // Log a debug message.
     void log::debug(const std::string& message)
     {
+        std::cout << message << std::endl;
     }
+
+    // Log an error message.
+    void log::error(const std::string& message)
+    {
+        std::cerr << message << std::endl;
+    }
+
 
 }
