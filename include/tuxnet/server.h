e4de34169d7444129a2bc4d114221a7387b1d66c
John Sennesael <johnsennesael@granular.ag>
2018-07-14 16:50:29 -0500

    update server docstrings

--- include/tuxnet/server.h
+++ include/tuxnet/server.h
@@ -54,8 +54,61 @@ namespace tuxnet
 
             // Events. --------------------------------------------------------
 
+            /**
+             * @brief on_connect event.
+             *
+             * Override this method in order to handle connecting clients.
+             *
+             * This event fires whenever a client connects.
+             *
+             * A pointer to a peer object is passed which allows you to get
+             * information on (and interact with) the client.
+             * 
+             * If you'd like to deny connections from a client, this would be a
+             * good place to do it.
+             *
+             * @param remote_peer : peer object representing the connecting 
+             *                      client.
+             */
             virtual void on_connect(peer* remote_peer);
+
+            /**
+             * @brief on_receive event.
+             *
+             * Override this method in order to handle incomming data from a
+             * client.
+             *
+             * This event fires whenever a client sends data.
+             *
+             * A pointer to a peer object is passed which allows you to get
+             * information on (and interact with) the client.
+             * 
+             * This is where you'd typically call remote_peer->read() or 
+             * similar functions to read data back from the client.
+             * 
+             * @param remote_peer : peer object representing the client which
+             *                      sent the data for which the event was
+             *                      triggered.
+             */
             virtual void on_receive(peer* remote_peer);
+
+            /**
+             * @brief on_disconnect event.
+             *
+             * Override this method in order to handle disconnecting clients.
+             *
+             * This event fires whenever a client disconnects gracefully, or
+             * when a client connection times out (if TCP keepalive is 
+             * enabled anyway - it is by default. See configure_keepalive() for
+             * details on TCP keepalive).
+             *
+             * This is where you would typically place cleanup code for
+             * disconnecting clients for instance.
+             *
+             * @param remote_peer : peer object representing the client that
+             *                      disconnected. 
+             *
+             */
             virtual void on_disconnect(peer* remote_peer);
 
     };
