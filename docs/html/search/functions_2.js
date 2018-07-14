8cee55d1d43038ae53f656b8b32ddf1c07635132
John Sennesael <johnsennesael@granular.ag>
2018-07-14 18:43:13 -0500

    documentation

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['close',['close',['../da/d32/classtuxnet_1_1socket.html#accb64eb3f3faac8e79a0e690deb55bdb',1,'tuxnet::socket']]]
+  ['close',['close',['../da/d32/classtuxnet_1_1socket.html#accb64eb3f3faac8e79a0e690deb55bdb',1,'tuxnet::socket']]],
+  ['configure_5fkeepalive',['configure_keepalive',['../d1/d32/classtuxnet_1_1server.html#afc9c0f8d4915331537ab326db3284eea',1,'tuxnet::server']]]
 ];
